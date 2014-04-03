7e024e5932ca14da4116291ede318c3129566eb9
Casey Falk <caseyfalk94@gmail.com>
2014-04-02 23:19:15 -0400

    Fixed bug in urls.py

--- GoBoard/urls.py
+++ GoBoard/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'GoBoard.views.main_page',),
-     url(r'^new_message$', 'GoBoard.views.new_message',),
      url(r'^home', 'GoBoard.views.main_page',),
-    url(r'^admin/', include(admin.site.urls)),
+
+     url(r'^new_message$', 'GoBoard.views.new_message',),
+     url(r'^get_messages$', 'GoBoard.views.send_messages',),
 )

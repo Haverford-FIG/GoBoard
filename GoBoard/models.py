ef6754492dd646ab047f23bb2ccb68e99b6d5443
Casey Falk <caseyfalk94@gmail.com>
2014-04-02 21:26:10 -0400

    Fixed some minor bugs.

--- GoBoard/models.py
+++ GoBoard/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth import user
+from django.contrib.auth.models import User
 
 class Message(models.Model):
 	user = models.ForeignKey(User, unique = False)
 	text = models.CharField(max_length=300)
-	tags_requiered = models.BooleanField
-	datetime = models.DateField([auto_now=False, auto_now_add=False])
+	tags_required = models.BooleanField()
+	datetime = models.DateTimeField()
 
 class Tags(models.Model):
 	tag = models.CharField(max_length=30)

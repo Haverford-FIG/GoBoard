0cd1d5599c30eaeba8c39bbe1ec97b1cee357dcb
Casey Falk <caseyfalk94@gmail.com>
2014-04-02 23:27:22 -0400

    Fixed bug in urls.py

--- GoBoard/urls.py
+++ GoBoard/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'GoBoard.views.main_page',),
-     url(r'^home', 'GoBoard.views.main_page',),
+     url(r'^home/?$', 'GoBoard.views.main_page',),
 
-     url(r'^new_message$', 'GoBoard.views.new_message',),
-     url(r'^get_messages$', 'GoBoard.views.send_messages',),
+     url(r'^new_message/?$', 'GoBoard.views.new_message',),
+     url(r'^get_messages/?$', 'GoBoard.views.send_messages',),
 
-     url(r'^get_tags$', 'GoBoard.views.get_tags',),
+     url(r'^get_tags/?$', 'GoBoard.views.get_tags',),
 )

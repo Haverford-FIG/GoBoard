1ae27ae5fe833ac14b0a5718603db555dc6bdffe
Casey Falk <caseyfalk94@gmail.com>
2014-04-16 21:05:45 -0400

    Halfway through adding server->client message display.

--- static/js/message.js
+++ static/js/message.js
@@ -1,4 +1,37 @@
+//Display a message on the message box.
+//Variable Axioms:
+// message = "This is a sample message";
+// tags = "#sample#message";
+
 $(document).ready(function() {
+
+function reloadMessages(messages, tags){
+	$(".messageBox").empty();
+	for (var i=0; i < messages.length ; i++){
+		displayMessage(messages[i], tags);
+	}
+}
+
+function displayMessage(message, tags) {
+	$(".messageBox").append(
+		"<div class=\"message\" tags=\""+tags+"\">"+message+"</div>"
+	);
+}
+
+function reloadMessages(tags) {
+	var json = {"tags": tags};
+	$("tagCheck").val(tags);
+	$.get('/get_messages/', json, function(response) {
+		//response = [message1, message2, ... ]
+		displayMessages(response);
+	});
+}
+
+    $("#tagSearchSubmit").on("click", function() {
+	tags = tags.replace(/ /g, "");
+	reloadMessages(tags);
+    });
+
     $("#submit").on("click", function() {
 	var tags = $("#tags").val();
 	var message = $("#message").val();
@@ -12,13 +45,12 @@ $(document).ready(function() {
 	var json = JSON.stringify(sendObj);
 	
 	$.post('/new_message/', json);
-    
-	$("#tags").val("");
-	$("#message").val("");
-    });
-});
 
-$(document).on("ready",function() {
+	reloadMessages(tags);
+    });
+
+
+
     var availableTags = [
       "Haverfest",
       "CSMajors"
